4444c54733aab43aa90ac892e08b7d3a5fd981b0
Roshan Singh <alcanivorax@users.noreply.github.com>
2026-01-21 21:48:31 +0530

    feat: list staged files

--- src/index.ts
+++ src/index.ts
@@ -13,11 +13,6 @@ function isInsideGitRepo(): boolean {
   }
 }
 
-if (!isInsideGitRepo()) {
-  console.error('Error: not inside a git repository')
-  process.exit(1)
-}
-
 function getStagedDiff(): string {
   try {
     return execSync('git diff --cached', {
@@ -30,7 +25,34 @@ function getStagedDiff(): string {
 
 const diff = getStagedDiff().trim()
 
+if (!isInsideGitRepo()) {
+  console.error('Error: not inside a git repository')
+  process.exit(1)
+}
+
 if (!diff) {
   console.error('No staged changed found.')
   process.exit(1)
+}
+
+function getStagedFiles(): string[] {
+  try {
+    const output = execSync('git diff --cached --name-only', {
+      encoding: 'utf8',
+    })
+
+    return output
+      .split('\n')
+      .map((f) => f.trim())
+      .filter(Boolean)
+  } catch {
+    return []
+  }
+}
+
+console.log('File changed:')
+const files = getStagedFiles()
+
+for (const file of files) {
+  console.log(`- ${file}`)
 }
