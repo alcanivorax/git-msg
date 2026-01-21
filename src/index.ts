dfb386021f27b64184f5de78a0affc6e937e29e3
Roshan Singh <alcanivorax@users.noreply.github.com>
2026-01-21 22:35:34 +0530

    feat: map change type to commit prefix

--- src/index.ts
+++ src/index.ts
@@ -50,9 +50,46 @@ function getStagedFiles(): string[] {
   }
 }
 
-console.log('File changed:')
 const files = getStagedFiles()
 
-for (const file of files) {
-  console.log(`- ${file}`)
+function detectChangeType(files: string[]): string {
+  const hasCode = files.some(
+    (f) => f.startsWith('src/') || f.endsWith('.ts') || f.endsWith('.js')
+  )
+  if (hasCode) return 'code'
+
+  const hasDocs = files.some((f) => f.startsWith('docs/') || f.endsWith('.md'))
+  if (hasDocs) return 'docs'
+
+  const hasConfig = files.some(
+    (f) =>
+      f === 'package.json' ||
+      f.endsWith('lock.yaml') ||
+      f.endsWith('config.js') ||
+      f.startsWith('.prettierc') ||
+      f.startsWith('.eslintrc') ||
+      f === 'tsconfig.json'
+  )
+
+  if (hasConfig) return 'config'
+
+  return 'chore'
 }
+
+const changeType = detectChangeType(files)
+
+function mapChangeTypeToPrefix(changeType: string): string {
+  switch (changeType) {
+    case 'code':
+      return 'feat'
+    case 'docs':
+      return 'docs'
+    case 'config':
+      return 'chore'
+    default:
+      return 'chore'
+  }
+}
+
+console.log('Detected change type: ', detectChangeType(files))
+console.log('Suggested commit prefix: ', mapChangeTypeToPrefix(changeType))
